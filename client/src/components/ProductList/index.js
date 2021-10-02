85a4c597ec7ad7a492fcf016daf70ddfebbc1c52
Xavy-Romeo <79165884+Xavy-Romeo@users.noreply.github.com>
2021-10-02 12:51:24 -0700

    heroku deploy

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -50,8 +50,6 @@ function ProductList() {
     return state.products.filter(product => product.category._id === currentCategory);
   };
 
-  console.log('state', state);
-
   return (
     <div className="my-2">
       <h2>Our Products:</h2>
